c7c2711c02fc0483a212c5872554000bf1805a3e
Daniel Mizsak <62774880+daniel-mizsak@users.noreply.github.com>
2025-01-12 02:56:25 +0100

    Try to eliminate build warnings

--- nix/modules/packages.nix
+++ nix/modules/packages.nix
@@ -12,18 +12,25 @@
       TERMINAL = "wezterm";
 
       # Required for Pyenv
-      # https://github.com/pyenv/pyenv/wiki/common-build-problems#build-failed-error-the-python-zlib-extension-was-not-compiled-missing-the-zlib
+      #   - https://semyonsinchenko.github.io/ssinchenko/post/using-pyenv-with-nixos/
+      #   - https://github.com/pyenv/pyenv/wiki/common-build-problems#build-failed-error-the-python-zlib-extension-was-not-compiled-missing-the-zlib
       CPPFLAGS =
         "-I${pkgs.bzip2.dev}/include "
         + "-I${pkgs.libffi.dev}/include "
+        + "-I${pkgs.ncurses.dev}/include "
         + "-I${pkgs.openssl.dev}/include "
         + "-I${pkgs.readline.dev}/include "
+        + "-I${pkgs.sqlite.dev}/include "
+        + "-I${pkgs.xz.dev}/include "
         + "-I${pkgs.zlib.dev}/include";
       LDFLAGS =
         "-L${pkgs.bzip2.out}/lib "
         + "-L${pkgs.libffi.out}/lib "
+        + "-L${pkgs.ncurses.out}/lib "
         + "-L${pkgs.openssl.out}/lib "
         + "-L${pkgs.readline.out}/lib "
+        + "-L${pkgs.sqlite.out}/lib "
+        + "-L${pkgs.xz.out}/lib "
         + "-L${pkgs.zlib.out}/lib";
       CONFIGURE_OPTS = "-with-openssl=${pkgs.openssl.dev}";
     };
@@ -67,6 +74,7 @@
       tmux
       tree
       wget
+      xz
       yazi
       zlib
       zoxide

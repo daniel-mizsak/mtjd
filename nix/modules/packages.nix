e45730b9d058ac434c18feb0dc5ca8a2d5bebb91
Daniel Mizsak <62774880+daniel-mizsak@users.noreply.github.com>
2025-01-13 02:09:44 +0100

    Feature/fix pyenv (#8)

--- nix/modules/packages.nix
+++ nix/modules/packages.nix
@@ -10,7 +10,33 @@
       EDITOR = "nvim";
       SUDO_EDITOR = "nvim";
       TERMINAL = "wezterm";
+
+      DOTNET_ROOT = "${pkgs.dotnet-sdk_8}/share/dotnet";
+
+      # Required for Pyenv
+      #   - https://semyonsinchenko.github.io/ssinchenko/post/using-pyenv-with-nixos/
+      #   - https://github.com/pyenv/pyenv/wiki/common-build-problems#build-failed-error-the-python-zlib-extension-was-not-compiled-missing-the-zlib
+      CPPFLAGS =
+        "-I${pkgs.bzip2.dev}/include "
+        + "-I${pkgs.libffi.dev}/include "
+        + "-I${pkgs.ncurses.dev}/include "
+        + "-I${pkgs.openssl.dev}/include "
+        + "-I${pkgs.readline.dev}/include "
+        + "-I${pkgs.sqlite.dev}/include "
+        + "-I${pkgs.xz.dev}/include "
+        + "-I${pkgs.zlib.dev}/include";
+      LDFLAGS =
+        "-L${pkgs.bzip2.out}/lib "
+        + "-L${pkgs.libffi.out}/lib "
+        + "-L${pkgs.ncurses.out}/lib "
+        + "-L${pkgs.openssl.out}/lib "
+        + "-L${pkgs.readline.out}/lib "
+        + "-L${pkgs.sqlite.out}/lib "
+        + "-L${pkgs.xz.out}/lib "
+        + "-L${pkgs.zlib.out}/lib";
+      CONFIGURE_OPTS = "-with-openssl=${pkgs.openssl.dev}";
     };
+
     systemPackages = with pkgs; [
       # Terminal
       _1password-cli
@@ -18,6 +44,7 @@
       atuin
       bat
       btop
+      bzip2
       deadnix
       direnv
       eza
@@ -27,23 +54,31 @@
       fzf
       gcc
       git
+      gnumake
       htop
       imagemagick
       jq
       kubectl
       lazydocker
       lazygit
+      libffi
+      ncurses
       neovim
       oh-my-posh
+      openssl
       poppler
       pre-commit
+      readline
       ripgrep
+      sqlite
       statix
       tlrc
       tmux
       tree
       wget
+      xz
       yazi
+      zlib
       zoxide
 
       dotnet-sdk_8

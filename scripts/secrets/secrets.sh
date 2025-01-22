897b4cd35cf03b5a21f0b7c6072a05bc484d0aad
Daniel Mizsak <62774880+daniel-mizsak@users.noreply.github.com>
2025-01-22 16:53:22 +0100

    Add basic secret management (#11)

---
+++ scripts/secrets/secrets.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash --packages bash
+
+# Read password in a hidden way
+read -sp "Enter your password: " SECRET
+echo -e "\n"
+
+# Encrypt by: `openssl enc -aes-256-cbc -in OP_SERVICE_ACCOUNT_TOKEN -out OP_SERVICE_ACCOUNT_TOKEN.enc -pbkdf2`
+OP_SERVICE_ACCOUNT_TOKEN=$(openssl enc -d -aes-256-cbc -in OP_SERVICE_ACCOUNT_TOKEN.enc -pass pass:"$SECRET" -pbkdf2 2>/dev/null)
+if [[ $? -ne 0 ]]; then
+    echo "Incorrect password. Exiting."
+    exit 1
+fi
+export OP_SERVICE_ACCOUNT_TOKEN
+
+op read --out-file ~/.ssh/id_github "op://Development/id_github/private key"

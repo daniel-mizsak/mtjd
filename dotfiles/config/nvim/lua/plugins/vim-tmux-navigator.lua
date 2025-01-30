0860fb02dbcb9cd9cf245866e439d8a4f0af6eff
Daniel Mizsak <62774880+daniel-mizsak@users.noreply.github.com>
2025-01-30 02:13:42 +0100

    Use alt instead of control for tmux-nvim navigation

--- dotfiles/config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ dotfiles/config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -8,10 +8,10 @@ return {
     "TmuxNavigatePrevious",
   },
   keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    { "<m-h>", "<cmd>TmuxNavigateLeft<cr>" },
+    { "<m-j>", "<cmd>TmuxNavigateDown<cr>" },
+    { "<m-k>", "<cmd>TmuxNavigateUp<cr>" },
+    { "<m-l>", "<cmd>TmuxNavigateRight<cr>" },
+    { "<m-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
   },
 }

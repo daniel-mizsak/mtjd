b50861b03c18ca963fc936187ab41a33768e770a
Daniel Mizsak <62774880+daniel-mizsak@users.noreply.github.com>
2025-03-04 20:24:04 +0100

    Remove garbage collect date

--- nix/mksystem.nix
+++ nix/mksystem.nix
@@ -38,7 +38,6 @@ in
         nix = {
           gc = {
             automatic = true;
-            dates = "weekly";
             options = "--delete-older-than 30d";
           };
           optimise.automatic = true;

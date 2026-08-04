26bf0617490e3f5ceca194025b89f5eab03f7eb9
Daniel Mizsak <62774880+daniel-mizsak@users.noreply.github.com>
2026-08-05 01:35:42 +0200

    Update neovim config (#85)
    * Update neovim to match vscode for python development
    
    * Update workflows, pre-commit hooks and packages

--- dotfiles/config/nvim/lua/config/options.lua
+++ dotfiles/config/nvim/lua/config/options.lua
@@ -1,4 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Use the system-provided ty language server for Python.
+vim.g.lazyvim_python_lsp = "ty"
+
 vim.opt.scrolloff = 10

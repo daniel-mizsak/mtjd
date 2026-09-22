19f5a6ee4fc650cabdd7462285c8a0b7cb4025a9
Daniel Mizsak <62774880+daniel-mizsak@users.noreply.github.com>
2026-09-22 10:42:18 +0200

    SSH key

--- nix/hosts/vm.nix
+++ nix/hosts/vm.nix
@@ -76,6 +76,9 @@
     ];
     home = "/home/${currentSystemUser}";
     shell = pkgs.fish;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIArEgeNCsIt8dFvyXKHKz1SYunldIoXXNAwTcRJTpbMc daniel@mizsak.com"
+    ];
   };
 
   # Do not change stateVersion after installation!
